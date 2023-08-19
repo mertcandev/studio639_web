24cc3f5d169f0ad448173680456d0f50a9191093
Mert Can YILMAZ <mertcanylmaz@gmail.com>
2023-08-19 23:44:49 +0300

    visual modifications

--- lib/pages/arch_aboutus_page.dart
+++ lib/pages/arch_aboutus_page.dart
@@ -21,138 +21,140 @@ class ArchAboutUsPage extends StatelessWidget {
         child: SizedBox(
           width: 1100,
           height: 750,
-          child: SingleChildScrollView(
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    width: 300,
-                    height: 600,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey.shade100,
-                        boxShadow: [
-                          const BoxShadow(
-                              color: Colors.white,
-                              offset: Offset(-5, -5),
-                              blurRadius: 5,
-                              spreadRadius: 5),
-                          BoxShadow(
-                              color: Colors.grey.shade300,
-                              offset: const Offset(5, 5),
-                              blurRadius: 5,
-                              spreadRadius: 5)
-                        ]),
-                    child: Padding(
-                      padding: const EdgeInsets.all(15),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 240,
-                            decoration: const BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: AssetImage(
-                                      "arch1.jpeg",
-                                    ))),
-                          ),
-                          const SizedBox(height: 20),
-                          Text(
-                            "studio 693",
-                            style: GoogleFonts.ptSans(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black),
-                          ),
-                          const SizedBox(height: 20),
-                          Text(
-                            onyazi,
-                            style: GoogleFonts.ptSans(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.grey),
-                          )
-                        ],
+          child: Center(
+            child: SingleChildScrollView(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      width: 300,
+                      height: 600,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey.shade100,
+                          boxShadow: [
+                            const BoxShadow(
+                                color: Colors.white,
+                                offset: Offset(-5, -5),
+                                blurRadius: 5,
+                                spreadRadius: 5),
+                            BoxShadow(
+                                color: Colors.grey.shade300,
+                                offset: const Offset(5, 5),
+                                blurRadius: 5,
+                                spreadRadius: 5)
+                          ]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 240,
+                              decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage(
+                                        "arch1.jpeg",
+                                      ))),
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              "studio 693",
+                              style: GoogleFonts.ptSans(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black),
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              onyazi,
+                              style: GoogleFonts.ptSans(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.grey),
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    width: 300,
-                    height: 600,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            colorFilter: ColorFilter.mode(
-                                Colors.grey.shade100, BlendMode.color),
-                            fit: BoxFit.cover,
-                            image: const AssetImage("furkan.png")),
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey.shade100,
-                        boxShadow: [
-                          const BoxShadow(
-                              color: Colors.white,
-                              offset: Offset(-5, -5),
-                              blurRadius: 5,
-                              spreadRadius: 5),
-                          BoxShadow(
-                              color: Colors.grey.shade300,
-                              offset: const Offset(5, 5),
-                              blurRadius: 5,
-                              spreadRadius: 5)
-                        ]),
-                  ),
-                  Container(
-                    padding:
-                        const EdgeInsets.only(top: 15, left: 15, right: 15),
-                    width: 300,
-                    height: 600,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey.shade100,
-                        boxShadow: [
-                          const BoxShadow(
-                              color: Colors.white,
-                              offset: Offset(-5, -5),
-                              blurRadius: 5,
-                              spreadRadius: 5),
-                          BoxShadow(
-                              color: Colors.grey.shade300,
-                              offset: const Offset(5, 5),
-                              blurRadius: 5,
-                              spreadRadius: 5)
-                        ]),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            bioIsim,
-                            style: GoogleFonts.ptSans(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black),
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            bioDetay,
-                            style: GoogleFonts.ptSans(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.black),
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            bioText,
-                            style: GoogleFonts.ptSans(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.grey),
-                          )
-                        ]),
-                  )
-                ]),
+                    Container(
+                      width: 300,
+                      height: 600,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Colors.grey.shade100, BlendMode.color),
+                              fit: BoxFit.cover,
+                              image: const AssetImage("furkan.png")),
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey.shade100,
+                          boxShadow: [
+                            const BoxShadow(
+                                color: Colors.white,
+                                offset: Offset(-5, -5),
+                                blurRadius: 5,
+                                spreadRadius: 5),
+                            BoxShadow(
+                                color: Colors.grey.shade300,
+                                offset: const Offset(5, 5),
+                                blurRadius: 5,
+                                spreadRadius: 5)
+                          ]),
+                    ),
+                    Container(
+                      padding:
+                          const EdgeInsets.only(top: 15, left: 15, right: 15),
+                      width: 300,
+                      height: 600,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey.shade100,
+                          boxShadow: [
+                            const BoxShadow(
+                                color: Colors.white,
+                                offset: Offset(-5, -5),
+                                blurRadius: 5,
+                                spreadRadius: 5),
+                            BoxShadow(
+                                color: Colors.grey.shade300,
+                                offset: const Offset(5, 5),
+                                blurRadius: 5,
+                                spreadRadius: 5)
+                          ]),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              bioIsim,
+                              style: GoogleFonts.ptSans(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black),
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              bioDetay,
+                              style: GoogleFonts.ptSans(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black),
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              bioText,
+                              style: GoogleFonts.ptSans(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.grey),
+                            )
+                          ]),
+                    )
+                  ]),
+            ),
           ),
         ),
       ),
